238d72c77ec56e8d4260053438a048987fc94f98
Lucas Torterotot <lucas.torterotot@gmail.com>
2021-01-04 15:40:06 +0100

    Fix directory creation for Imperial FF

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -52,7 +52,7 @@ cd ../..
 git clone git@github.com:KIT-CMS/fake-factor-application.git HiggsAnalysis/fake-factor-application
 
 # Imperial FF weights
-mkdir HiggsAnalysis/friend-tree-producer/data/imperial_ff
+mkdir -p HiggsAnalysis/friend-tree-producer/data/imperial_ff
 cd HiggsAnalysis/friend-tree-producer/data/imperial_ff
 scp -r ${USERNMLXP}@lxplus.cern.ch:/afs/cern.ch/user/g/guttley/public/fake_factors_mssm/* ./
 
