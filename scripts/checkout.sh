f5e37d644c5c8540f3abfb8c4a793973a045f6b9
Janek Bechtel <janek.bechtel@kit.edu>
2021-03-24 11:16:00 +0100

    Update checkout.sh

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -46,7 +46,7 @@ git checkout v0.2.2
 
 read -p "lxplus-username: " USERNMLXP
 
-rsync -vhrP /ceph/htautau/fakefactor_files/sm/* .
+rsync -vhrP /ceph/htautau/fakefactor_files/sm_new/* .
 
 cd ../..
 git clone git@github.com:KIT-CMS/fake-factor-application.git HiggsAnalysis/fake-factor-application
