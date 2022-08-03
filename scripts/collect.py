ad4c33f386897f9aa1dd50374843d5e8a9b65d4c
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-08-03 14:22:48 +0200

    improve collection step

--- scripts/collect.py
+++ scripts/collect.py
@@ -18,26 +18,29 @@ def write_trees_to_files(info):
     era = info[3]
     channel = info[4]
     # from the nick, remove the last _#number
-    nick = nick.replace("_{}".format(nick.split("_")[-1]), "")
-    print "Copying trees for %s" % nick
-    print("Nick: {}, collection_path: {}, db: {}, era {}, channel {}".format(nick, collection_path, db, era, channel))
+    foldername = nick.replace("_{}".format(nick.split("_")[-1]), "")
     # the outputpath follows the pattern era/channel/nick/files.root
-    nick_path = os.path.join(collection_path, era, channel, nick)
+    nick_path = os.path.join(collection_path, era, channel, foldername)
     if not os.path.exists(nick_path):
         os.makedirs(nick_path)
     outputfile = os.path.join(nick_path, nick + ".root")
     # now merge all files in db["files"] into the output file using hadd
     if len(db["files"]) > 1:
-        print "Merging %s" % nick
-        hadd_cmd = ["hadd", outputfile]
+        print("Merging Nick: {}, Channel: {}, Era: {}".format(nick, channel, era))
+        hadd_cmd = ["hadd -f", outputfile]
         # unpack the filenames to total paths
         for filepath in db["files"]:
             full_filepath = os.path.join(collection_path, filepath)
             hadd_cmd.append(full_filepath)
-        print(" ".join(hadd_cmd))
-        subprocess.call(hadd_cmd)
+        try:
+            output = subprocess.check_output(" ".join(hadd_cmd), shell=True)
+            # use decode function to convert to string
+            # print('###############')
+            # print('Output:', output.decode("utf-8"))
+        except subprocess.CalledProcessError as error:
+            print('Error code:', error.returncode, '. Output:', error.output.decode("utf-8"))
     else:
-        print "Copying %s" % nick
+        print("Copying Nick: {}, Channel: {}, Era: {}".format(nick, channel, era))
         try:
             shutil.copy(db["files"][0], outputfile)
         except IOError:
@@ -73,7 +76,7 @@ def collect_outputs(executable, cores, custom_workdir_path, mode):
             first = jobdb[str(jobnumber)][subjobnumber]["first_entry"]
             last = jobdb[str(jobnumber)][subjobnumber]["last_entry"]
             filename = "_".join([nick, folder, str(first), str(last)]) + ".root"
-            filepath = os.path.join(workdir_path, nick, filename)
+            filepath = os.path.join(workdir_path, "{}_{}_{}".format(era, channel, nick), filename)
             if nick not in datasetdb:
                 datasetdb[nick] = {}
                 if era not in datasetdb[nick]:
@@ -97,12 +100,18 @@ def collect_outputs(executable, cores, custom_workdir_path, mode):
 
 
     if mode == "local":
+        commands = []
         nicks = sorted(datasetdb)
         for nick in nicks:
             for era in datasetdb[nick]:
                 for channel in datasetdb[nick][era]:
-                    write_trees_to_files([nick, collection_path, datasetdb[nick][era][channel], era, channel])
-        # pool = Pool(cores)
+                    commands.append([nick, collection_path, datasetdb[nick][era][channel], era, channel])
+                    # write_trees_to_files([nick, collection_path, datasetdb[nick][era][channel], era, channel])
+        print("Using {} cores to collect outputs".format(cores))
+        pool = Pool(cores)
+        pool.map(write_trees_to_files, commands)
+    else:
+        raise NotImplementedError("Mode {} is not implemented".format(mode))
         # pool.map(
         #     write_trees_to_files,
         #     zip(nicks, [collection_path] * len(nicks), [datasetdb] * len(nicks)),
