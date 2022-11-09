bf5f199a19178a8a444339a904791365374424e7
Maximilian Burkart <maxiburkart@gmail.com>
2022-11-09 15:10:25 +0100

    Fix mass assignment for mt channel

--- bin/SVFit.cc
+++ bin/SVFit.cc
@@ -221,11 +221,11 @@ int main(int argc, char** argv)
         // determine the right mass convention for the TauLepton decay products
         Float_t mass_1, mass_2;
         if(ditaudecay.first == MeasuredTauLepton::kTauToElecDecay)        mass_1 = 0.51100e-3;
-        else if(ditaudecay.first == MeasuredTauLepton::kTauToElecDecay)   mass_1 = 105.658e-3;
+        else if(ditaudecay.first == MeasuredTauLepton::kTauToMuDecay)   mass_1 = 105.658e-3;
         else                                                              mass_1 = m_1;
 
         if(ditaudecay.second == MeasuredTauLepton::kTauToElecDecay)       mass_2 = 0.51100e-3;
-        else if(ditaudecay.second == MeasuredTauLepton::kTauToElecDecay)  mass_2 = 105.658e-3;
+        else if(ditaudecay.second == MeasuredTauLepton::kTauToMuDecay)  mass_2 = 105.658e-3;
         else                                                              mass_2 = m_2;
 
         // define lepton four vectors
